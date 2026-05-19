d5e3d012c4f7a6e9d29aa489d506cd0edd30e258
Braden Kim <bradenkim22@gmail.com>
2026-05-18 21:07:41 -0600

    adding DataAccess to server

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,18 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import java.util.Collection;
+
+public interface DataAccess {
+    void clear() throws DataAccessException;
+
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+
+    void createAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+    
+}
