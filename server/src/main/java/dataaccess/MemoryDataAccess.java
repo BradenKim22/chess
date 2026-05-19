570218a31568d03643b437e3dc1bf3b6a1edd31d
Braden Kim <bradenkim22@gmail.com>
2026-05-19 15:36:57 -0600

    memorydataaccess

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -29,4 +29,58 @@ public class MemoryDataAccess implements DataAccess {
     public void createUser(UserData user) {
         users.put(user.username(), user);
     }
+
+    @Override
+    public UserData getUser(String username) {
+        return users.get(username);
+    }
+
+    @Override
+    public void createAuth(AuthData auth) {
+        authTokens.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        return authTokens.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) {
+        authTokens.remove(authToken);
+    }
+
+    @Override
+    public int createGame(String gameName) {
+        int gameID = nextGameID;
+        nextGameID++;
+
+        GameData game = new GameData(
+                gameID,
+                null,
+                null,
+                gameName,
+                new ChessGame()
+        );
+
+        games.put(gameID, game);
+
+        return gameID;
+    }
+
+    @Override
+    public GameData getGame(int gameID) {
+        return games.get(gameID);
+    }
+
+    @Override
+    public Collection<GameData> listGames() {
+        return games.values();
+    }
+
+    @Override
+    public void updateGame(GameData game) {
+        games.put(game.gameID(), game);
+    }
+
 }
