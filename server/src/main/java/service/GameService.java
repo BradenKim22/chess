ff16d35ccba5540fee6561b4fee7bc22be1a5e37
Braden Kim <bradenkim22@gmail.com>
2026-05-19 16:15:13 -0600

    GameService joinGame

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -35,4 +35,47 @@ public class GameService {
 
         return dataAccess.createGame(gameName);
     }
+
+    public void joinGame(String authToken, String playerColor, int gameID) throws DataAccessException {
+        AuthData auth = dataAccess.getAuth(authToken);
+
+        if (auth == null) {
+            throw new DataAccessException("unauthorized");
+        }
+
+        GameData game = dataAccess.getGame(gameID);
+
+        if (game == null || playerColor == null) {
+            throw new DataAccessException("bad request");
+        }
+
+        String whiteUsername = game.whiteUsername();
+        String blackUsername = game.blackUsername();
+
+        if (playerColor.equals("WHITE")) {
+            if (whiteUsername != null) {
+                throw new DataAccessException("already taken");
+            }
+
+            whiteUsername = auth.username();
+        } else if (playerColor.equals("BLACK")) {
+            if (blackUsername != null) {
+                throw new DataAccessException("already taken");
+            }
+
+            blackUsername = auth.username();
+        } else {
+            throw new DataAccessException("bad request");
+        }
+
+        GameData updatedGame = new GameData(
+                game.gameID(),
+                whiteUsername,
+                blackUsername,
+                game.gameName(),
+                game.game()
+        );
+
+        dataAccess.updateGame(updatedGame);
+    }
 }
