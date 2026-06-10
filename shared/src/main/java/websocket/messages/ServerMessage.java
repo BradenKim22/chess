fd41438107b2ea14e5056cf38b0274adad5d56c2
Braden Kim <bradenkim22@gmail.com>
2026-06-10 17:41:08 -0600

    Load Message

--- shared/src/main/java/websocket/messages/ServerMessage.java
+++ shared/src/main/java/websocket/messages/ServerMessage.java
@@ -3,13 +3,12 @@ package websocket.messages;
 import java.util.Objects;
 
 /**
- * Represents a Message the server can send through a WebSocket
- * <p>
- * Note: You can add to this class, but you should not alter the existing
- * methods.
+ * Represents a message the server can send through a WebSocket.
+ * Note: You can add to this class, but you should not alter the existing methods.
  */
 public class ServerMessage {
-    ServerMessageType serverMessageType;
+
+    private final ServerMessageType serverMessageType;
 
     public enum ServerMessageType {
         LOAD_GAME,
