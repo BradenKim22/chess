bc2d4bc2368a0a10d3b702274ee873038ca6c48a
Braden Kim <bradenkim22@gmail.com>
2026-06-10 17:36:20 -0600

    UserGameCommand edits

--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -1,25 +1,33 @@
 package websocket.commands;
 
+import chess.ChessMove;
+
 import java.util.Objects;
 
 /**
- * Represents a command a user can send the server over a websocket
- * <p>
- * Note: You can add to this class, but you should not alter the existing
- * methods.
+ * Represents a command a user can send the server over a websocket.
+ *
+ * Note: You can add to this class, but you should not alter the existing methods.
  */
 public class UserGameCommand {
 
     private final CommandType commandType;
-
     private final String authToken;
-
     private final Integer gameID;
+    private final ChessMove move;
 
     public UserGameCommand(CommandType commandType, String authToken, Integer gameID) {
         this.commandType = commandType;
         this.authToken = authToken;
         this.gameID = gameID;
+        this.move = null;
+    }
+
+    public UserGameCommand(String authToken, Integer gameID, ChessMove move) {
+        this.commandType = CommandType.MAKE_MOVE;
+        this.authToken = authToken;
+        this.gameID = gameID;
+        this.move = move;
     }
 
     public enum CommandType {
@@ -41,6 +49,10 @@ public class UserGameCommand {
         return gameID;
     }
 
+    public ChessMove getMove() {
+        return move;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -49,13 +61,14 @@ public class UserGameCommand {
         if (!(o instanceof UserGameCommand that)) {
             return false;
         }
-        return getCommandType() == that.getCommandType() &&
-                Objects.equals(getAuthToken(), that.getAuthToken()) &&
-                Objects.equals(getGameID(), that.getGameID());
+        return getCommandType() == that.getCommandType()
+                && Objects.equals(getAuthToken(), that.getAuthToken())
+                && Objects.equals(getGameID(), that.getGameID())
+                && Objects.equals(getMove(), that.getMove());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getCommandType(), getAuthToken(), getGameID());
+        return Objects.hash(getCommandType(), getAuthToken(), getGameID(), getMove());
     }
 }
