cef39581a9cb291249360ba236714fb269f62f22
Braden Kim <bradenkim22@gmail.com>
2026-06-10 17:54:18 -0600

    connection

--- server/src/main/java/server/websocket/Connection.java
+++ server/src/main/java/server/websocket/Connection.java
@@ -1,4 +1,32 @@
 package server.websocket;
 
+import io.javalin.websocket.WsContext;
+
 public class Connection {
+
+    private final String username;
+    private final int gameID;
+    private final WsContext session;
+
+    public Connection(String username, int gameID, WsContext session) {
+        this.username = username;
+        this.gameID = gameID;
+        this.session = session;
+    }
+
+    public String username() {
+        return username;
+    }
+
+    public int gameID() {
+        return gameID;
+    }
+
+    public WsContext session() {
+        return session;
+    }
+
+    public void send(String message) {
+        session.send(message);
+    }
 }
