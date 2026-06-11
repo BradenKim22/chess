a015755222471546418dddc2b9a4dd40c36c7e94
Braden Kim <bradenkim22@gmail.com>
2026-06-10 18:11:21 -0600

    connectionmanager

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,4 +1,35 @@
 package server.websocket;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ConnectionManager {
+
+    private final Set<Connection> connections = new HashSet<>();
+
+    public void add(Connection connection) {
+        connections.add(connection);
+    }
+
+    public void remove(Connection connection) {
+        connections.remove(connection);
+    }
+
+    public void removeBySession(String sessionId) {
+        connections.removeIf(connection ->
+                connection.session().sessionId().equals(sessionId)
+        );
+    }
+
+    public Set<Connection> getConnectionsForGame(int gameID) {
+        Set<Connection> result = new HashSet<>();
+
+        for (Connection connection : connections) {
+            if (connection.gameID() == gameID) {
+                result.add(connection);
+            }
+        }
+
+        return result;
+    }
 }
